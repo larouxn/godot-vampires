f789104d8c3331893012a8b09014fb0353de4df3
Nicholas La Roux <larouxn@gmail.com>
2024-04-21 20:36:17 +0900

    Fix slime collision and set up walk animation

--- mob.gd
+++ mob.gd
@@ -6,3 +6,6 @@ func _physics_process(_delta):
 	var direction = global_position.direction_to(player.global_position)
 	velocity = direction * 300.0
 	move_and_slide()
+
+	if velocity.length() > 0.0:
+		%Slime.play_walk()
